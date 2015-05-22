65ac89ec7ebdc3af13a0bf58a2e9a185306c0960
velkan14 <velkan14@gmail.com>
2015-05-23 00:27:36 +0100

    Libgdx created

---
+++ desktop/src/co/bravebunny/miniclipjam/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package co.bravebunny.miniclipjam.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import co.bravebunny.miniclipjam.MyCoffeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyCoffeGame(), config);
+	}
+}
